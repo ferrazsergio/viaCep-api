49b23b30217b557214db8826474b79d43505756f
Sérgio Ferraz <93234341+ferrazsergio@users.noreply.github.com>
2024-05-09 14:19:53 -0300

    chore: deletando diretorio

--- src/main/java/br/com/projeto/via/cep/exceptions/CepInvalidoException.java
+++
@@ -1,8 +0,0 @@
-package br.com.projeto.via.cep.exceptions;
-
-public class CepInvalidoException extends RuntimeException{
-    private static final long serialVersionUID = 1L;
-    public CepInvalidoException(String msg) {
-        super(msg);
-    }
-}
